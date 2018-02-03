4a0b9b12747a962cddd596dd2b699860b1970f7f
Ben Callaghan <benj.callaghan@gmail.com>
2018-02-03 15:11:57 -0700

    Created an add method

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export class Transaction {
+export interface Task {
+    execute(context: any): Promise<any> | any;
+    rollback(context: any): Promise<void> | void;
+}
 
+export class Transaction {
+    private stage: number = -1;
+    private tasks: Task[] = [];
+
+    /**
+     * Add one or more tasks to be executed as part of this transaction
+     * @param tasks One or more tasks to be added
+     */
+    public add(...tasks: Task[]): void {
+        this.tasks.push(...tasks);
+    }
 }
